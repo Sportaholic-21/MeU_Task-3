d2fc104e668daf52680e926b3054002744073791
Long Nguyen <18521044@gm.uit.edu.vn>
2022-04-12 13:52:38 +0700

    fix day format

--- helpers/dateInput.js
+++ helpers/dateInput.js
@@ -1,7 +1,13 @@
 module.exports.dateInput = (date) => {
     let inputDate = date.split(/[-\/]/)
-    if (inputDate[0] > 12) {
-        inputDate[0] = [inputDate[1], inputDate[1] = inputDate[0]][0]
+    let dd = 0
+    console.log(inputDate[0].toString())
+    if (inputDate[0].toString().length > 2) {
+        console.log("here")
+        dd = 1
+    }
+    if (inputDate[dd] > 12) {
+        inputDate[dd] = [inputDate[dd + 1], inputDate[dd + 1] = inputDate[dd]][0]
     }
     return new Date(inputDate.join("/"))
 }
